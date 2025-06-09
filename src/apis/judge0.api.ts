6efb647d897bfc98f7125862250f49dd4d73b29f
Nguyen Thanh Nam <nvthanhnam121@gmail.com>
2025-06-09 15:57:19 +0700

    fix: judge0 core api

--- src/apis/judge0.api.ts
+++ src/apis/judge0.api.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export const runCode = async (code: string, languageId: number, stdin: string, expected_output: string) => {
   // const response = await axios.post('http://localhost:2358/submissions?base64_encoded=false&wait=true', {
-  const response = await axios.post('http://14.225.207.77:2358/submissions?base64_encoded=false&wait=true', {
+  // const response = await axios.post('http://14.225.207.77:2358/submissions?base64_encoded=false&wait=true', {
+  const response = await axios.post('http://eurekas-judge0.io.vn:2358//submissions?base64_encoded=false&wait=true', {
     language_id: languageId,
     source_code: code,
     stdin,
